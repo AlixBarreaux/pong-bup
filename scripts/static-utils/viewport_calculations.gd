03165e163ebb9846d580f16c359b10a61151b17a
AlixBarreaux <alix@github.com>
2024-06-29 15:42:03 +0200

    Refactor: Change type to RefCounted
    Instead of Node because this class is not supposed to be instanced.

--- scripts/static-utils/viewport_calculations.gd
+++ scripts/static-utils/viewport_calculations.gd
@@ -1,9 +1,10 @@
-extends Node
+extends RefCounted
 class_name ViewportCalculations
 
 
 static func get_viewport_width() -> float:
 	return ProjectSettings.get_setting("display/window/size/viewport_width")
+
 
 static func get_viewport_height() -> float:
 	return ProjectSettings.get_setting("display/window/size/viewport_height")
