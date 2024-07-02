15c3c450afb813d56d4d49fd62fc5f2e52186d68
AlixBarreaux <alix@github.com>
2024-07-02 17:21:43 +0200

    Fix: Paddle ref for ball
    Ball uses new functions to calculate width, height and half of each but paddle.gd was forgotten on the last commit

--- scripts/paddle.gd
+++ scripts/paddle.gd
@@ -1,2 +1,24 @@
 extends CharacterBody2D
 class_name Paddle
+
+
+@export var speed: float = 200.0
+
+
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var coll_shape: Shape2D = collision_shape_2d.get_shape()
+@onready var coll_shape_size: Vector2 = coll_shape.get_size()
+
+
+func get_width() -> float:
+	return coll_shape_size.x
+
+func get_height() -> float:
+	return coll_shape_size.y
+
+
+func get_half_width() -> float:
+	return self.get_width() / 2
+
+func get_half_height() -> float:
+	return self.get_height() / 2
