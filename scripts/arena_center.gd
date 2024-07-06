0452bcbc5b4ada458d87a7c635bf0248f768a505
AlixBarreaux <alix@github.com>
2024-07-06 18:10:30 +0200

    Refactor: Remove arena_center
    Marker2D in Level + script attached to it.

--- scripts/arena_center.gd
+++
@@ -1,9 +0,0 @@
-extends Marker2D
-class_name ArenaCenter
-
-
-func _ready() -> void:
-	var half_viewport_width: float = ViewportCalculations.get_half_viewport_width()
-	var half_viewport_height: float = ViewportCalculations.get_half_viewport_height()
-	
-	self.set_global_position(Vector2(half_viewport_width, half_viewport_height))
