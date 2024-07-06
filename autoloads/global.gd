d3b278589862fb8e8e3d19b100489a1ecdf94a2a
AlixBarreaux <alix@github.com>
2024-07-06 18:38:19 +0200

    Feat: Reset all team scores
    On new_game_started all team scores are resets to 0

--- autoloads/global.gd
+++ autoloads/global.gd
@@ -19,33 +19,43 @@ var team_info: Dictionary = {
 }
 
 
+# Value: Increased BY
+signal score_increased(team_id: int, value: int)
+# Value: New updated score
+signal score_changed(team_id: int, value: int)
+
+
+func reset_all_team_scores() -> void:
+	var score_reset_value: int = 0
+	
+	for team in teams:
+		self.set_score_of_team(team.id, 0)
+
+
+func set_score_of_team(id: int, new_score: int) -> void:
+	self.score_changed.emit(id, new_score)
+
+
+func increase_score_of_team(_last_hit_by_team_id: int, cage_owner_team_id: int, value: int = 1) -> void:
+	for team in teams:
+		if team.id != cage_owner_team_id:
+			self.set_score_of_team(team.id, value)
+			self.score_increased.emit(team.id, value)
+
+
+func on_new_game_started() -> void:
+	self.reset_all_team_scores()
+
+
 func on_goal_scored(last_hit_by_team_id: int, cage_owner_team_id: int, value: int) -> void:
 	increase_score_of_team(last_hit_by_team_id, cage_owner_team_id, value)
 
 
 func _ready() -> void:
+	Events.new_game_started.connect(on_new_game_started)
 	Events.goal_scored.connect(on_goal_scored)
 	
 	for count in team_count:
 		var new_team_info = team_info.duplicate(true)
 		new_team_info.id = count + 1
 		teams.append(new_team_info)
-
-# Value: Increased BY
-signal score_increased(team_id: int, value: int)
-# Value: New updated score
-signal score_changed(team_id: int, value: int)
-
-func increase_score_of_team(_last_hit_by_team_id: int, cage_owner_team_id: int, value: int) -> void:
-	var team_id_receiving_points: int = 0
-	var updated_score: int = 0
-	
-	for team in teams:
-		if team.id != cage_owner_team_id:
-			team.score += value
-			
-			team_id_receiving_points = team.id
-			updated_score = team.score
-	
-	self.score_increased.emit(team_id_receiving_points, value)
-	self.score_changed.emit(team_id_receiving_points, updated_score)
